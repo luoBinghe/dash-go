daf8d66d7e2df984ce66e708271c6db64a84fe5c
guayana <isabelaguayanaz@gmail.com>
2021-11-09 17:50:16 -0300

    inserindo regarregamento para o cashing (visual)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,15 +4,27 @@ import { useQuery } from 'react-query'
 import { Sidebar } from '../../components/Sidebar'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
-import Link from 'next/link'
-import { useEffect } from 'react'
+import Link from 'next/link' 
 
 export default function UserList(){
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users;
   })
 
   const isWideVersion = useBreakpointValue({
@@ -30,6 +42,11 @@ export default function UserList(){
             <Flex mb="8" justify="space-between" align="center"> 
               <Heading size="lg" fontWeight="normal">
                 Usuários
+
+                {!isLoading && isFetching && (
+                  <Spinner size="sm" color="gray.500" ml="4" />
+                )}
+
               </Heading>
               <Link href="/users/create" passHref> 
                 <Button 
@@ -65,18 +82,21 @@ export default function UserList(){
                       </Tr>
                     </Thead>
                     <Tbody>
-                      <Tr>
-                        <Td px={["4", "4", "6"]} color="gray.300" width="8">
-                          <Checkbox  colorScheme="pink" />
-                        </Td>
-                        <Td>
-                          <Box>
-                            <Text fontWeight="bbold">Isabela G</Text>
-                            <Text fontSize="sm" color="gray.300">isabelaguayanaz@gmail.com</Text>
-                          </Box>
-                        </Td>
-                        { isWideVersion && <Td>04 de Abril de 2021</Td> }
-                      </Tr>
+                      { data.map((user) => (
+                        <Tr key={user.id}>
+                          <Td px={["4", "4", "6"]} color="gray.300" width="8">
+                            <Checkbox  colorScheme="pink" />
+                          </Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight="bold">{user.name}</Text>
+                              <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                            </Box>
+                          </Td>
+                          { isWideVersion && <Td>{user.createdAt}</Td> }
+                        </Tr>
+                      ))
+                      }
                     </Tbody>
                   </Table>
                   <Pagination />
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -28,7 +28,7 @@ export function makeServer(){
     },
 
     seeds(server){
-      server.createList('user', 30)
+      server.createList('user', 10)
     },
     
     routes(){
