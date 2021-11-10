65f809f5b10089b190bf4ebddb6588db64ea8c2c
guayana <isabelaguayanaz@gmail.com>
2021-11-10 16:19:13 -0300

    configurando o axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,13 @@ import { Sidebar } from '../../components/Sidebar'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import Link from 'next/link' 
+import { api } from '../../services/api'
 
 export default function UserList(){
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+    const { data } = await api.get('users')
+
+    console.log('data', data)
 
     const users = data.users.map(user => {
       return {
