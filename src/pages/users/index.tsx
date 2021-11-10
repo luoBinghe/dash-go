2568506da0a57c795f710a1c3e705dd1aae61217
guayana <isabelaguayanaz@gmail.com>
2021-11-10 17:41:58 -0300

    paginação e hook de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,33 +1,13 @@
 import { Spinner, Flex, Box, Heading, Button, Icon, Table, Thead, Tr, Th,  Tbody, Checkbox, Td, Text, useBreakpointValue } from '@chakra-ui/react'
 import { RiAddLine } from 'react-icons/ri'
-import { useQuery } from 'react-query'
 import { Sidebar } from '../../components/Sidebar'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import Link from 'next/link' 
-import { api } from '../../services/api'
+import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UserList(){
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('users')
-
-    console.log('data', data)
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users;
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
